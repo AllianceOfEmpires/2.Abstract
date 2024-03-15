d7a55997019ffd1e54bd5fc624db15d34338f1bb
eternityspace <rodon@protonmail.ch>
2024-03-15 20:20:47 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='useful',
+    version='1',
+    description='Very useful code',
+    url='http://github.com/dummy_user/useful',
+    author='Flying Circus',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['markdown'],
+    # entry_points={'console_scripts': ['run = __main__:run']}
+    entry_points={'console_scripts': ['run = src.__main__:run']}
+)
